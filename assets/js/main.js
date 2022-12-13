8522dab633ed689644bfcf193e5a2506b05279bc
LRoyez <lroyez.lamanu@gmail.com>
2022-12-13 15:06:17 +0100

    changement front

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,80 +1,84 @@
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-    
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show-menu')
-        })
-    }
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
 }
-showMenu('nav-toggle','nav-menu')
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-const navLink = document.querySelectorAll('.nav__link')
+function scrollHeader() {
+  const nav = document.getElementById("header");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-
-    navMenu.classList.remove('show-menu')
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+window.addEventListener("scroll", scrollHeader);
 
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
 
-function scrollHeader(){
-    const nav = document.getElementById('header')
-
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollTop);
 
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
 
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollTop)
-
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
-
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
-
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
 
 if (selectedTheme) {
-
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
 
-themeButton.addEventListener('click', () => {
-
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
-
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '10px',
-    duration: 1000,
-    reset: true
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-sr.reveal(`.home__data, .home__img,
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "10px",
+  duration: 1000,
+  reset: true,
+});
+
+sr.reveal(
+  `.home__data, .home__img,
             .about__data, .about__img,
             .exp__content, .portfolio__content,
             .app__data, .app__img,
             .contact__data, .contact__button,
-            .footer__content`, {
-    interval: 300
-})
+            .footer__content`,
+  {
+    interval: 200,
+  }
+);
